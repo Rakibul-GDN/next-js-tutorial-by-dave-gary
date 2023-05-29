d5a37999b3d82eb5c1ef385de4d298c8acfa76a7
VooMoo <rakib.7896@gmail.com>
2023-05-29 08:18:32 +0600

    Followed video 1 on, Next.js Tutorial for Beginners | Next.js 13

---
+++ next01/.next/static/webpack/app/page.e692b64cd2eff4a5.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CNext%20Projects%5CNext%2013%20By%20Dave%20Gray%5Cnext01%5Capp%5Cpage.module.css&server=false!":
+/*!******************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CNext%20Projects%5CNext%2013%20By%20Dave%20Gray%5Cnext01%5Capp%5Cpage.module.css&server=false! ***!
+  \******************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./app/page.module.css */ \"(app-client)/./app/page.module.css\", 23))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPUQlM0ElNUNOZXh0JTIwUHJvamVjdHMlNUNOZXh0JTIwMTMlMjBCeSUyMERhdmUlMjBHcmF5JTVDbmV4dDAxJTVDYXBwJTVDcGFnZS5tb2R1bGUuY3NzJnNlcnZlcj1mYWxzZSEuanMiLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz82ZDYxIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiRDpcXFxcTmV4dCBQcm9qZWN0c1xcXFxOZXh0IDEzIEJ5IERhdmUgR3JheVxcXFxuZXh0MDFcXFxcYXBwXFxcXHBhZ2UubW9kdWxlLmNzc1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CNext%20Projects%5CNext%2013%20By%20Dave%20Gray%5Cnext01%5Capp%5Cpage.module.css&server=false!\n"));
+
+/***/ })
+
+});
